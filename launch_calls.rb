695e7b95400d7699a30a41c1f7ca9cf0d266ebe3
Jason Goecke <jason@goecke.net>
2009-06-24 19:51:01 -0700

    Using a different iterator

--- launch_calls.rb
+++ launch_calls.rb
@@ -1,15 +1,16 @@
 #!/usr/bin/ruby
 require 'lib/launcher'
 
+cnt = 0
 p '*'*50
-@config["number_of_calls"].each_with_index do |index|
+@config["number_of_calls"].times do
   result = call_into_context(@config["channel"], 
                             @config["context"], 
                             { :variables => { "call_launcher" => "true",
                                               "sleep_time"    => @config["sleep_time"],
                                               "file_to_play"  => @config["file_to_play"] } })
                                             
-  p "Launched call number #{(index + 1).to_s}..."
+  p "Launched call number #{cnt += 1).to_s}..."
   p result
   p '*'*50
 end
