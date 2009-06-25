42d1620e35fc7eccd6e1a14538e44f09ba77877e
Jason Goecke <jason@goecke.net>
2009-06-25 08:57:39 -0700

    Fixed variables to post properly.

--- launch_calls.rb
+++ launch_calls.rb
@@ -4,11 +4,12 @@ require 'lib/launcher'
 cnt = 0
 p '*'*50
 @config["number_of_calls"].times do
-  result = @adhearsion.call_into_context(@config["channel"], 
-                                         @config["context"], 
-                                         { :variables => { "call_launcher" => "true",
-                                                           "sleep_time"    => @config["sleep_time"],
-                                                           "file_to_play"  => @config["file_to_play"] } })
+  result = @adhearsion.originate({ :channel   => @config["channel"], 
+                                   :context    => @config["context"], 
+                                   :extension => '1000',
+                                   :priority  => 1,
+                                   :callerid  => "TopicTelTest",
+                                   :variable  => "call_launcher=true|sleep_time=#{@config['sleep_time']}|file_to_play=#{@config['file_to_play']}" })
                                             
   p "Launched call number #{(cnt += 1).to_s}..."
   p result
